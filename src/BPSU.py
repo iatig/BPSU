5cdfeba91424a7991e0662f5e8ead473ba326727
Itai Arad <ia@fastmail.com>
2024-06-29 10:46:04 +0800

    Initial files

---
+++ src/BPSU.py
@@ -0,0 +1,660 @@
+#-----------------------------------------------------------------------
+#
+#               BUSP.py            VERSION 0.1
+#    ==================================================
+#
+#  Functions to perform Imaginary/Real Time Evolution (ITE) using
+#  Simple Update (SU), with a BP gauge fixing.
+#
+#
+#
+#=======================================================================
+
+
+
+import numpy as np
+import scipy
+
+from numpy.linalg import norm, svd, qr
+
+from numpy import zeros, ones, array, tensordot, sqrt, diag, conj, \
+	eye, trace, pi, exp, isnan
+
+
+HERMICITY_ERR = 1e-4
+PINV_THRESH = 1e-12
+ROBUST_THRESH = 1e8
+
+
+#
+# ---------------------------- sqrt_message  ---------------------------
+#
+
+def sqrt_message(m):
+	
+	"""
+	
+	Given a message m (which is a PSD matrix), calculate m^{1/2}, m^{-1/2}.
+	
+	Do that in a robust way by first diagonalizing, and then removing
+	parts of the spectrum that are smaller than some threshold.
+	
+	"""
+	
+	#
+	# Diagonalize
+	#
+	evals, U = np.linalg.eigh(m)
+	
+	#
+	# The eigenvalues threshold: ignore the space of eigenvalues smaller 
+	# than that.
+	#
+	
+	thresh = evals[-1]*PINV_THRESH
+	i = np.where(evals>thresh)[0][0]
+	evals_red = evals[i:]
+	U_red = U[:,i:]
+	
+	# 
+	# Calculate m^{1/2}, and m^{-1/2}
+	#
+	M_sq = U_red@diag(sqrt(evals_red))@conj(U_red.T)
+	Minv_sq = U_red@diag(evals_red**(-0.5))@conj(U_red.T)
+	
+
+	return M_sq, Minv_sq
+
+
+
+
+#
+# ------------------------ contract_leg  -------------------------------
+#
+
+def contract_leg(T, g, leg):
+	
+	newT = tensordot(T, g, axes=([leg+1],[0]))
+	L = len(T.shape)
+	perm = list(range(leg+1)) + [L-1] + list(range(leg+1,L-1))
+	newT = newT.transpose(perm)
+	
+	return newT
+	
+
+#
+# ------------------------  edge_BP_gauging  ---------------------------
+#
+
+def edge_BP_gauging(T1, leg1, T2, leg2, m12, m21):
+	"""
+	
+	Given two neighboring tensors, T1, T2, with a common edge, together
+	with the two incoming/outgoing BP messages between them, perform 
+	a re-gauging of their common leg to bring it to the Vidal gauge, 
+	and calculate the weight of that edge.
+	
+	After the regauging, the TN looks like:
+	
+	  newT1      w      newT2
+	----O--------o--------O----
+	    |                 |
+	
+	
+	and satisfies the Vidal gauge condition:
+	
+	         newT1
+	      +---O----        +----
+	     /    |            |
+	m01 O     |      =     |
+	     \    |            |
+	      +---O----        +----
+	         newT1*
+	         
+	         
+	Input Parameters:
+	------------------
+	
+	T1, T2 --- The tensors on  which we work
+	
+	leg1   --- The number of the leg in T1 that connects to T2, 0 being
+	           the first leg, etc
+	           
+	leg2   --- Like leg1, but for T2
+	
+	m12    --- The T1 => T2 converged BP message
+	
+	m21    --- The T2 => T1 converged BP message
+	
+	Output:
+	---------
+	
+	newT1, w, newT2 --- the new T1,T2, together with the Vidal weights.
+	
+	
+	"""
+	
+	#
+	# First calculate the square-root and its inverse for both BP 
+	# messages.
+	#
+	m12_sq, m12inv_sq = sqrt_message(m12)
+	m21_sq, m21inv_sq = sqrt_message(m21)
+	
+	#
+	# Create the matrix in the middle
+	#
+	M = m12_sq.T@m21_sq
+	
+	U,s,V = svd(M, full_matrices=False)
+		
+	#
+	# Calculate g1, g2 --- the gauge trans we apply to leg1,leg2 in T1, T2
+	#
+	g1 = m12inv_sq.T@U
+	
+	g2 = m21inv_sq.T@V.T
+		
+	#
+	# Apply g1, g2 to T1, T2 and obtain newT1, newT2
+	#
+	
+	newT1 = contract_leg(T1, g1, leg1)
+	
+	newT2 = contract_leg(T2, g2, leg2)
+	
+	return newT1, s, newT2
+	
+
+
+
+#
+# ---------------------------  BP_gauging  -----------------------------
+#
+
+def BP_gauging(T_list, e_dict, m_list):
+	"""
+	
+	Give a TN described by T_list, e_dict, together with a converged set
+	of BP messages m_list, move the TN into the Vidal gauge in which 
+	at the middle of every edge we place a diagonal weight tensor.
+	
+	We are following "Gauging tensor networks with belief propagation",  
+	Joseph Tindall and Matt Fishman, SciPost Phys. 15, 222 (2023) here.
+	
+	Input Parameters:
+	-----------------
+	T_list --- The list of tensors that make up the TN
+	
+	e_dict --- The edges dictionary. The key is the edge name. The value
+	           for an edge e=(i,j) is a 4-taple (i, leg_i, j, leg_j) 
+	           
+	m_list --- The converged BP messages. For every neighboring vertices
+	           i,j, m_list[i][j] is the converged i=>j BP  message.
+	
+	
+	Output:
+	---------
+	
+	T_list --- The updated T_list
+	w_dict --- A dictionary holding the weights of the Vidal gauge for
+	           ever edge e.
+	
+	"""
+	
+	#
+	# first, copy T_list to a new list
+	#
+	
+	gauged_T_list = T_list.copy()
+	
+	
+	w_dict = {}
+	
+	for e in e_dict.keys():
+		
+		vi, i_leg, vj, j_leg = e_dict[e]
+				
+		new_Ti, w_e, new_Tj = edge_BP_gauging(gauged_T_list[vi], i_leg, \
+			gauged_T_list[vj], j_leg, m_list[vi][vj], m_list[vj][vi])
+		
+		gauged_T_list[vi] = new_Ti
+		gauged_T_list[vj] = new_Tj
+		
+		# Normalize the weights
+		w_dict[e] = w_e/sum(w_e)
+		
+		
+		
+	return gauged_T_list, w_dict
+	
+#
+# ---------------------------  merge_SU_weights  -----------------------
+#
+
+def merge_SU_weights(T_list, e_dict, w_dict):
+	
+	"""
+	
+	Merge the SU weights back into the TN tensors. Each weight is split
+	into 2 by taking a square, and then we swallow each part at a 
+	neighboring tensor.
+	
+	"""
+	
+	merged_T_list = T_list.copy()
+	
+	for e in e_dict.keys():
+		
+		w = w_dict[e]
+		sqw = sqrt(abs(w))
+		sqM = diag(sqw)
+		
+		i1,leg1, i2,leg2 = e_dict[e]
+		
+		T1 = contract_leg(merged_T_list[i1], sqM, leg1)
+		T2 = contract_leg(merged_T_list[i2], sqM, leg2)
+		
+		merged_T_list[i1] = T1
+		merged_T_list[i2] = T2
+		
+	return merged_T_list
+		
+#
+# --------------------------   gather_ext_legs   -----------------------
+#
+
+def gather_ext_legs(T, leg):
+	
+	"""
+	
+	Permute legs into [D0, D1, ..., d, Di] and then fuse the first Ds, 
+	so that we get: [Drest, d, Di] ==> [Drest, d*Di]
+	
+	In addition, return the shape of the tensor before we coarse-grain
+	the indices, so that we will know later how to undo this.
+	
+	
+	"""
+	
+	L = len(T.shape)
+	
+	perm = list(range(L))
+	perm.remove(0)
+	perm.remove(leg+1)
+	
+	perm = perm + [0,leg+1]
+	
+	
+	dD = T.shape[0]*T.shape[leg+1]
+	Drest = T.size//dD
+	
+	M = T.transpose(perm)
+	sh = list(M.shape)
+	M = M.reshape([Drest, dD])
+	
+	return M, sh
+	
+	
+#
+# ---------------------------  apply_2local_gate -----------------------
+#
+
+def apply_2local_gate(T_list, e_list,  e_dict, w_dict, g, e, \
+	Dmax=None, eps=None):
+		
+	"""
+	
+	Given a TN in the Vidal gauge, apply a 2-body gate g on the tensors 
+	of a given edge and truncate the bond dimension to Dmax using the 
+	Simple-Update framework.
+	
+	A detailed explanation of the algorithm can be found at 
+	"Universal tensor-network algorithm for any infinite lattice",
+	PRB 99, 195105 (2019) 
+	
+	Input Parameters:
+	------------------
+	
+	T_list, e_list, e_dict, w_dict --- The description of the TN
+	
+	g    --- The 2-local gate, given as [i1,j1; i2,j2] where j1,j2 are 
+	         the ket legs and i1,i2 are the bra legs.
+	      
+  e    --- The label of the edge on which the gate is acting
+           note that e=(i,j) where i<j
+  
+  Dmax --- The maximal final bond dimension. If not given, no truncation
+           is done.
+           
+  eps  --- Another truncation criteria. If given, all singular values
+           that are < s_max*eps are truncated. If given in conjuncation 
+           with Dmax, then the minimal bond dimension is used.
+  
+          
+  Output:
+  --------
+  
+  T_list, w_dict --- Tensors of the updated TN.
+	      
+	
+	
+	"""
+	
+	#
+	# Locate the vertices of the edge e=(i1,i2) and their tensors T1, T2
+	#
+	
+	
+	i1,leg1, i2,leg2 = e_dict[e]
+		
+	T1 = T_list[i1]
+	T2 = T_list[i2]
+	w = w_dict[e]
+	
+	D = T1.shape[leg1+1]  # Original dimension of the common leg
+	d1 = T1.shape[0]  # physical leg T1
+	d2 = T2.shape[0]  # physical leg T2
+	
+	# ---------------------------------------------------------------
+	# 1. Absorb all the weights of T1, T2 into these tensors (except
+	#    for the weight of the common leg
+	# ---------------------------------------------------------------
+		
+	es1 = e_list[i1]
+	for leg,f in enumerate(es1):
+		
+		if f==e:
+			continue
+		
+		w_mat = diag(w_dict[f])
+		
+		T1 = contract_leg(T1, w_mat, leg)
+
+	es2 = e_list[i2]
+	for leg,f in enumerate(es2):
+		
+		if f==e:
+			continue
+		
+		w_mat = diag(w_dict[f])
+		
+		T2 = contract_leg(T2, w_mat, leg)
+	
+	# -----------------------------------------------------------------
+	# 2. Reshape T1, T2 into matrices, where one leg is the fusion of 
+	#    all non-participating legs, and the second is (d,D), where 
+	#    d is the physical leg and D is the common leg
+	# -----------------------------------------------------------------
+	
+	M1, T1_shape = gather_ext_legs(T1, leg1)
+	M2, T2_shape = gather_ext_legs(T2, leg2)
+
+
+	# -----------------------------------------------------------------
+	# 3. Perfrom QR on M1, M2 to separate (d,D) legs from the rest.
+	# -----------------------------------------------------------------
+	
+	Q1,R1 = qr(M1)
+	Q2,R2 = qr(M2)
+		
+	# -------------------------------------------------
+	# 4. Separate d from the common leg in R1, R2
+	# -------------------------------------------------
+	
+	R1 = R1.reshape([R1.shape[0], d1, D])
+	R2 = R2.reshape([R2.shape[0], d2, D])
+	
+	# -------------------------------------------------
+	# 5. Contract: R1 + R2 + w + g
+	# -------------------------------------------------
+	
+	#
+	# First, contract R1 with the SU weight
+	#
+	
+	R1 = tensordot(R1, diag(w), axes=([2],[0]))
+	
+	#
+	# Second, contract R1 with the gate g.
+	# 
+	#  R1 shape: [RestL, d1, D]
+	#  g  shape: [i1, j1; i2, j2]
+	# 
+	#  We contract d1<-->j1
+	#
+	#
+
+	R1 = tensordot(R1, g, axes=([1], [1]))
+	#
+	# R1 form: [RestL, D, i1, i2, j2]
+	#
+	# R2 form: [RestR, d2, D]
+	#
+	# Now contract with R2 along D<-->D and d2<-->j2
+	#
+	R12 = tensordot(R1, R2, axes=([1, 4], [2,1]))
+	
+	# Final R12 form: [RestL, i1, i2, RestR]
+	
+	#
+	# 6. Turn R12 into a matrix, and SVD it
+	#
+  
+	sh = R12.shape
+	
+	R12 = R12.reshape([sh[0]*sh[1], sh[2]*sh[3]])
+		
+	U, s, V = svd(R12, full_matrices=False)
+	
+	D_full = len(s)
+	
+	# -------------------------------------------------
+	# 7. Truncate (if needed)
+	# -------------------------------------------------
+	if D_full>Dmax or eps is not None:
+		
+		if eps is not None:
+			#
+			# If eps is given, then we truncate all singular 
+			# values < s_max*eps. s_max = s[0]
+			#
+			i = np.where(s<s[0]*eps)[0][0]
+			if i<Dmax:
+				Dmax = i
+		
+		#
+		# Re-define the unitaries U,V and the weights s to contain only
+		# the non-truncated values
+		# 
+		s = s[:Dmax]
+		U = U[:,:Dmax]
+		V = V[:Dmax, :]
+	
+	#
+	# Normalize the final SU weights
+	#
+	s = s/sum(s)
+	
+		
+	# -------------------------------------------------
+	# 8. Open up the d legs in U,V
+	# -------------------------------------------------
+	
+	sh = U.shape
+	D = sh[1]  # Final bond dimension
+	
+	U = U.reshape([sh[0]//d1, d1, sh[1]])
+	# U shape: [RestL, d, D]
+	
+	
+	sh = V.shape
+	V = V.reshape([sh[0], d2, sh[1]//d2])
+	# V shape: [D, d,  RestR]
+	V = V.transpose([2,1,0])
+	# V shape: [RestR, d, D]
+	
+	
+	# -------------------------------------------------
+	# 9. Contract U<-->Q1 and V<-->Q2
+	# -------------------------------------------------
+	
+	Q1 = tensordot(Q1, U, axes=([1],[0]))
+	Q2 = tensordot(Q2, V, axes=([1],[0]))
+	
+	# Q1 shape: [RestL, d, D]     Q2: [RestR, d, D]
+	
+	# -------------------------------------------------
+	# 10. Separete the rest of the legs in Q1, Q2
+	# -------------------------------------------------
+	T1_shape[-1] = D
+	T2_shape[-1] = D
+	
+	T1 = Q1.reshape(T1_shape)
+	T2 = Q2.reshape(T2_shape)
+	
+	# T1 shape: other-legs, d1, D
+	# T2 shape: other-legs, d2, D
+	
+	# -------------------------------------------------
+	# 11. Re-arrange the legs of T1, T2
+	# -------------------------------------------------
+	
+	sh = T1.shape
+	L = len(T1.shape)
+	perm = [L-2] + list(range(leg1)) + [L-1] + list(range(leg1,L-2))
+	T1 = T1.transpose(perm)
+	
+	sh = T2.shape
+	L = len(T2.shape)
+	perm = [L-2] + list(range(leg2)) + [L-1] + list(range(leg2,L-2))
+	T2 = T2.transpose(perm)
+	
+	# -----------------------------------------------------
+	# 12. Remove the SU weights from the rest of the legs 
+	# -----------------------------------------------------
+	
+	es1 = e_list[i1]
+	for leg,f in enumerate(es1):
+		
+		if f==e:
+			continue
+		
+		smax = w_dict[f][0]*PINV_THRESH
+		k = w_dict[f].shape[0]
+		w_mat = diag(1/(w_dict[f] + smax*ones(k)))
+		
+		T1 = contract_leg(T1, w_mat, leg)
+
+	es2 = e_list[i2]
+	for leg,f in enumerate(es2):
+		
+		if f==e:
+			continue
+		
+		smax = w_dict[f][0]*PINV_THRESH
+		k = w_dict[f].shape[0]
+		w_mat = diag(1/(w_dict[f] + smax*ones(k)))
+		
+		T2 = contract_leg(T2, w_mat, leg)
+
+	
+	# -----------------------------------------------------------
+	# 13. Update T1, T2, w in the T_list, w_dict list/dictionary
+	# -----------------------------------------------------------
+		
+	T_list[i1] = T1
+	T_list[i2] = T2
+	w_dict[e] = s
+
+	
+	return T_list, w_dict
+	
+	
+#
+# -------------------------  local_2RDMs  ------------------------------
+#
+
+def local_2RDMs(T_list, e_list,  e_dict, w_dict):
+	
+	rho_dict={}
+	
+	for e in e_dict.keys():
+		v1,leg1, v2,leg2 = e_dict[e]
+		
+		T1 = T_list[v1]
+		T2 = T_list[v2]
+		w = w_dict[e]
+	
+		D = T1.shape[leg1+1]  # Original dimension of the common leg
+		d1 = T1.shape[0]  # physical leg T1
+		d2 = T2.shape[0]  # physical leg T2
+	
+		#
+		# Absorb all the weights of T1, T2 into these tensors (except
+		# for the weight of the common leg
+		# 
+			
+		es1 = e_list[v1]
+		for leg,f in enumerate(es1):
+			
+			
+			w_mat = diag(w_dict[f])
+			
+			T1 = contract_leg(T1, w_mat, leg)
+
+		#
+		# contract T1 with the bra along all legs except for the connecting
+		# one
+		#
+		L = len(T1.shape)
+		sh = list(range(L))
+		sh.remove(0)
+		sh.remove(leg1+1)
+		
+		T1ketbra = tensordot(T1, conj(T1), axes=(sh, sh))
+		# T1ketbra form: d, D, d*, D*
+		
+
+		es2 = e_list[v2]
+		for leg,f in enumerate(es2):
+			
+			if f==e:
+				continue
+			
+			w_mat = diag(w_dict[f])
+			
+			T2 = contract_leg(T2, w_mat, leg)
+			
+		#
+		# contract T2 with the bra along all legs except for the connecting
+		# one
+		#
+		L = len(T2.shape)
+		sh = list(range(L))
+		sh.remove(0)
+		sh.remove(leg2+1)
+			
+		T2ketbra = tensordot(T2, conj(T2), axes=(sh, sh))
+		# T2ketbra form: d, D, d*, D*
+		
+		
+		#
+		# get rho12 by contracting T1ketbra with T2ketbra along D,D*
+		#
+			
+		rho12 = tensordot(T1ketbra, T2ketbra, axes=([1,3],[1,3]))
+		
+		tr = trace(rho12, axis1=0, axis2=1)
+		tr = trace(tr, axis1=0, axis2=1)
+		
+		rho12 = rho12/tr
+		
+		rho_dict[e] = rho12
+		
+		
+	
+	
+	return rho_dict
+	
+	
---
+++ src/example-BP-SU.py
@@ -0,0 +1,338 @@
+#!/usr/bin/python3
+#----------------------------------------------------------
+#
+#                  example-BP-SU.py
+#             =======================
+#
+#  This program uses the BP-SU algorithm to:
+#
+#  1. Run BP
+#  2. Use the converged messages to calculate <obs>_11 --- the expectation
+#     value of the obs := |+><+| on site 11
+#
+#  3. Use the messages to calculate the Vidal gauge
+#
+#  4. Calculate <obs>_11 using the Vidal gauge
+# 
+#  5. Apply a 2-local gate (C-NOT) on sites 11,9 using the Vidal gauge
+#
+#  6. Absorb the Vidal weights into the TN
+#  
+#  7. Re-run the BP
+#
+#  8. Use the converged BP messages to re-calculate <obs>_11
+#
+#
+# History:
+# ---------
+# 
+# 28-Jun-2024   Initial version
+#
+#----------------------------------------------------------
+#
+
+
+import numpy as np
+
+from numpy.linalg import norm
+
+from numpy import zeros, ones, array, tensordot, sqrt, diag, dot, \
+	reshape, transpose, conj, eye, trace, pi
+	
+
+from qbp import qbp, calc_e_dict
+
+from BPSU import apply_2local_gate, BP_gauging, merge_SU_weights
+
+
+#
+# ------------------------- create_example  ---------------------------
+#
+
+def create_example_TN(d, D):
+	
+	"""
+	 We create a random 12 spins PEPS according to the example in 
+	 the document.
+	 
+	 Input Parameters:
+	 ------------------
+	 d --- Physical bond dimension
+	 D --- Logical bond dimension
+	 	 
+	 	 
+	 Output:
+	 -------
+	 
+	 The T_list, e_list of the random TN
+	 
+
+	"""
+	
+	
+	
+	#           T0        T1       T2       T3          T4
+	e_list = [ [1,2], [1,3,19,6], [5,3], [2,5,4,14], [19,4,7,8,20], 
+	#   T5           T6        T7          T8               9
+	[6,7,10], [8,10,9,11], [9,12], [11,20,15,18,17], [16,14,15],
+	#  10            11
+	[13,17,12], [16,18,13]]
+	
+	
+	# The list of the original PEPS tensors. First index in each tensor
+	# is the physical leg.
+	T_list = []
+
+
+	#
+	# Create the local tensors as random tensors
+	#
+	
+	for eL in e_list:
+		k = len(eL)
+		sh = [d] + [D]*k
+		
+		T = np.random.normal(size=sh) + 1j*np.random.normal(size=sh)
+		T = T/norm(T)
+		
+		T_list.append(T)
+		
+	return T_list, e_list
+
+
+#
+# -------------------------  calc_rho_i  -------------------------------
+#
+
+def calc_rho_i(T, in_messages):
+	
+	"""
+	
+	Given a PEPS tensor T a corresponding list of incoming BP messages, 
+	calculate the BP approximation of the reduced density matrix on that
+	site
+	
+	
+	"""
+	
+	m = len(in_messages)
+	
+	T_ket = T.copy()
+	
+	#
+	# Contract the incoming messages to T_ket
+	#
+	
+	for i in range(m):
+		T_ket = tensordot(T_ket, in_messages[i], axes=([1],[0]))
+		
+	legs = list(range(1,m+1))
+	
+	rho = tensordot(T_ket, conj(T), axes=(legs, legs))
+	
+	rho = rho/trace(rho)
+	
+	return rho
+	
+	
+
+
+#
+#
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+#                             M  A  I  N
+#
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#
+#
+
+
+def main():
+	
+	print("\n\n\n")
+	print("   EXAMPLE OF BP-SU: ")
+	print("   =================\n")
+	print("  Run BP on a random PEPS, use it to move to the Vidal gauge,")
+	print("  calculate local expectation value, and apply a 2-local gate\n\n")
+	
+	np.random.seed(2)
+	
+	d = 2  # physical dimension
+	D = 5  # PEPS bond dimension
+	
+	#
+	# BP parameters
+	#
+	BP_MAX_ITER  = 50
+	BP_DELTA     = 1e-6
+	BP_INITIAL_M = 'U'
+	BP_DAMPING   = 0
+	
+	#
+	# The observable for which we are going to calculate the expectation
+	# value
+	#
+	obs = array([[1,1],[1,1]])/2  # = |+><+|
+	
+	#
+	# Create the TN from the example with random tensors
+	#
+	T_list, e_list = create_example_TN(d, D)
+	e_dict = calc_e_dict(e_list)
+	
+	#
+	# =======================   STEP 1: Run BP   =========================
+	#
+	print("\n")
+	print("Step 1: Running BP")
+	print("------------------------------------------------\n")
+	
+	m_list, err, iter_no = qbp(T_list, e_list, e_dict, \
+		initial_m=BP_INITIAL_M, max_iter=BP_MAX_ITER, \
+		delta=BP_DELTA, damping=BP_DAMPING)
+		
+	print(f"Done with {iter_no} iterations and convergence err {err:.6g}")
+		
+	#
+	# =====================   STEP 2: Calc <obs>   =======================
+	#
+	
+	#
+	# Calc the RDM rho_i for i=11. Vertex 11 has edges [16,18,13], which 
+	# correspond to neighboring vertices [9,8,10]
+	#
+	print("\n")
+	print("Step 2: Calc <obs>_11 using the converged BP messages")
+	print("----------------------------------------------------\n")
+	
+	T = T_list[11]
+	
+	#
+	# Define a list of incoming messages to vertex 11
+	#
+	in_messages = [m_list[9][11], m_list[8][11], m_list[10][11]]
+	
+	rho_BP = calc_rho_i(T, in_messages)
+	
+	#
+	# Once we have the RDM, we can easily calculate the local expectation
+	# value
+	#
+	BP_av = trace(rho_BP@obs)
+	BP_av = BP_av.real
+	
+	print(f"BP average: <obs>_11 = {BP_av:6g}")
+	
+	#
+	# ==============  STEP 3: Calculate the Vidal Gauge   ================
+	#
+	
+	#
+	# The BP messages are used to calculate the Vidal gauge, which gives
+	# us canonical tensors + edge weights
+	#
+	
+	print("\n")
+	print("Step 3: Calc the Vidal gauge")
+	print("------------------------------\n")
+	
+	
+	gauged_T_list, w_dict = BP_gauging(T_list, e_dict, m_list)
+
+	#
+	# ==========  STEP 4: Use the Vidal Gauge to calc <obs>  =============
+	#
+
+	#
+	# In the Vidal gauge the effective messages are diagonal with 
+	# the square of the weights on the diagonal. The edges of vertex 11
+	# are [16,18,13], so we use their weights.
+
+	print("\n")
+	print("Step 4: Use the Vidal gauge to calc <obs>_11")
+	print("--------------------------------------------\n")
+
+	
+	T = gauged_T_list[11]
+	in_messages = [diag(w_dict[16]**2), diag(w_dict[18]**2), \
+		diag(w_dict[13]**2) ]
+	rho_BP = calc_rho_i(T, in_messages)
+	
+	Vidal_av = trace(rho_BP@obs)
+	Vidal_av = Vidal_av.real
+	
+	print(f"Vidal gauge average: <obs>_11 = {Vidal_av:6g}")
+	
+	#
+	# ==========  STEP 5: apply C-NOT on (9,11)  =============
+	#
+
+	print("\n")
+	print("Step 5: Use the Vidal gauge to apply C-NOT(9,11)")
+	print("------------------------------------------------\n")
+
+	
+	#
+	# first, create the 2-local tensor that represents the CNOT
+	#
+	X = array([[0,1],[1,0]])
+	ketbra00 = array([[1,0],[0,0]])
+	ketbra11 = array([[0,0],[0,1]])
+	CNOT = tensordot(ketbra00, eye(2), 0) + tensordot(ketbra11, X, 0) 
+	
+	#
+	# The ID of the (9,11) edge is 16, and since 9<11, then 9 is the 
+	# first qubit and 11 is the second qubit. So 9 will be the control
+	# and 11 will be the target in the C-NOT gate.
+	#
+	
+	gauged_T_list, w_dict = apply_2local_gate(gauged_T_list, e_list, \
+		e_dict, w_dict, g=CNOT, e=16, Dmax=D)
+		
+	#
+	# Now gauged_T_list is the updated tensors with an approx Vidal gauge
+	#
+	
+	#
+	# ==========  STEP 6: merge the Vidal weights back into the TN =======
+	#
+
+	print("\n")
+	print("Step 6: merge the Vidal weights back into the TN")
+	print("---------------------------------------------------\n")
+
+	T_list = merge_SU_weights(gauged_T_list, e_dict, w_dict)
+	
+	#
+	# ==========  STEP 7: re-run BP =======
+	#
+	print("\n")
+	print("Step 7: re-run BP")
+	print("---------------------------------------------------\n")
+	
+	m_list, err, iter_no = qbp(T_list, e_list, e_dict, \
+		initial_m=BP_INITIAL_M, max_iter=BP_MAX_ITER, \
+		delta=BP_DELTA, damping=0)
+		
+	print(f"Done with {iter_no} iterations and convergence err {err:.6g}")
+	
+	#
+	# ===============  STEP 8: Re-calculate <obs>_11   =================
+	#
+	print("\n")
+	print("Step 8: Recalculate <obs>_11")
+	print("------------------------------\n")
+	
+	T = T_list[11]
+	in_messages = [m_list[9][11], m_list[8][11], m_list[10][11]]
+	
+	rho_BP = calc_rho_i(T, in_messages)
+	
+	BP_av = trace(rho_BP@obs)
+	BP_av = BP_av.real
+	
+	print(f"BP average: <obs>_11 = {BP_av:6g}")
+	
+	
+main()
